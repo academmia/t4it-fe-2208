7bb161be90661b79916b3909a68e18dd9789f82d
unknown <academmia.com@gmail.com>
2022-08-10 22:58:36 +0300

    [t4it-fe-00002] clean project - add routing - read url param

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,6 +1,16 @@
+import { useRouter } from "next/router";
+
 const ProjectPage = () => {
-  return (
-    <h1>Project Page</h1>
-  )
-}
-export default ProjectPage
+ 
+    const router = useRouter();
+
+    console.log(router);
+
+    return (
+        <>
+            <h1>Project</h1>
+            <h3>{router.query.id}</h3>
+        </>
+    )
+};
+export default ProjectPage;
