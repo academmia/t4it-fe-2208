01a2ebeea029c58c1308fc457d4a6cd78ae3975d
academmia <46922652+academmia@users.noreply.github.com>
2022-08-27 08:40:03 +0300

    [t4it-fe-00006] home, projects mock data/api, ProjectItem component (#6)

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,8 +1,27 @@
-const ProjectsPage = () => {
+import Layout from "@/components/core/layout/Layout";
+import { API_URL } from "@/config/index"
+import ProjectItem from "@/components/projects/ProjectItem";
+
+export default function ProjectsPage({ projects }) {
   return (
     <Layout>
-      <h1>Projects Page</h1>
+      <h1>Projects</h1>
+      { projects.length === 0 && <h3>No projects...</h3> }
+      { projects.map( p => (
+        <ProjectItem key={p.id} project={p}/>
+      ) ) }
     </Layout>
-  )
+  );
 }
-export default ProjectsPage
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/projects`);
+  const projects = await res.json();
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 1
+  }
+}
